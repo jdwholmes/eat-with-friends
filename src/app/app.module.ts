e0506c09347bb3cec7fa0e4671697dd8458a6257
Jack Holmes <jdwholmes@gmail.com>
2017-12-11 16:21:02 +0000

    Fixing my error from weeks ago

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,48 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { DashboardComponent } from './dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './login/logout.component';
+import { LoginFullComponent } from './login/login-full.component';
+import { RegisterComponent } from './user/register.component';
+import { UserDetailComponent } from './user/user-detail.component';
+import { OrderComponent } from './order/order.component';
+import { OrderDetailComponent } from './order/order-detail.component';
+import { SideMenuComponent } from './menu/side-menu.component';
+import { MyOrdersComponent } from './order/my-orders.component';
+
+import { LoginService } from './login/login.service';
+import { UserService } from './user/user.service';
+import { AppConfig } from './app.config';
+
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserDetailComponent,
+    LoginFullComponent,
+    LogoutComponent,
+    OrderComponent,
+    OrderDetailComponent,
+    MyOrdersComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    LoginService,
+    AppConfig,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
